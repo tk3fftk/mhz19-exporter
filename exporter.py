9085f4c0ede94f37f5279a1865d9bfd8ece54d15
tk3fftk <tktk.stereoman@gmail.com>
2020-08-09 14:44:06 +0900

    add failure counter

--- exporter.py
+++ exporter.py
@@ -1,10 +1,11 @@
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Counter
 import random
 import time
 import serial
 import mh_z19
 
 CO2 = Gauge('co2_concentration', 'CO2 concentration (ppm)')
+failure_count = Counter('co2_obtaining_failure', '# of failure obtaining CO2')
 INTERVAL = 5
 
 
@@ -16,6 +17,8 @@ def obtain_co2():
     else:
         if val:
             CO2.set(val['co2'])
+        else:
+            failure_count.inc()
 
 
 if __name__ == '__main__':
