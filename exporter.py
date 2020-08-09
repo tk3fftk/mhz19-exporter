612b90d4c50f0750bb02b702c73f46f09cc3941f
Hiroki Takatsuka <tktk.stereoman@gmail.com>
2020-08-09 15:01:27 +0900

    Handling (#2)
    * add error handling
    
    * add failure counter

--- exporter.py
+++ exporter.py
@@ -1,15 +1,24 @@
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Counter
 import random
 import time
+import serial
 import mh_z19
 
 CO2 = Gauge('co2_concentration', 'CO2 concentration (ppm)')
+failure_count = Counter('co2_obtaining_failure', '# of failure obtaining CO2')
 INTERVAL = 5
 
 
 def obtain_co2():
-    val = mh_z19.read()
-    CO2.set(val['co2'])
+    try:
+        val = mh_z19.read()
+    except serial.SerialException as e:
+        print("error", e)
+    else:
+        if val:
+            CO2.set(val['co2'])
+        else:
+            failure_count.inc()
 
 
 if __name__ == '__main__':
